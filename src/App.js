5ac7fdc9db8ac89b6e271d3be28d936c02908b96
parkyes90 <parkyes90@gmail.com>
2020-03-11 16:28:24 +0900

    Add hooks useTitle.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
 import React from "react";
-import useTabs from "./hooks/useTabs";
-
-const content = [
-  { tab: "Section 1", content: "I'am the content of the Section 1" },
-  { tab: "Section 2", content: "I'am the content of the Section 2" }
-];
+import useTitle from "./hooks/useTitle";
 
 function App() {
-  const { currentItem, changeItem } = useTabs(0, content);
+  const setTitle = useTitle("Loading...");
   return (
     <div>
-      {content.map((section, index) => (
-        <button key={section.tab} onClick={() => changeItem(index)}>
-          {section.tab}
-        </button>
-      ))}
-      <div>{currentItem.content}</div>
+      <input
+        type="text"
+        onChange={e => {
+          setTitle(e.target.value);
+        }}
+      />
     </div>
   );
 }
